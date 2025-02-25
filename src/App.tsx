fa35d325db7f5c8b4973c1c54ca71fb8524c6970
VictorKevz <victor.kuwandira@student.oulu.fi>
2025-02-25 12:02:19 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main className='flex flex-col items-center justify-center min-h-screen bg-gray-700'>
+      <h1 className='text-3xl text-white font-bold'>Start Building with a CMS</h1>
+    </main>
+  )
+}
+
+export default App
